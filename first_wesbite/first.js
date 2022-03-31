50da7da7171d5db49f7ac0aa54d73e1413d6d2dc
Abdullah-Mohammed-OTU <abdullah.mohammed@ontariotechu.net>
2022-03-31 16:17:59 -0400

    New repo

---
+++ first_wesbite/first.js
@@ -0,0 +1,8 @@
+function main() { 
+    var addition1 = parseInt(prompt("Enter a number: "));
+    var addition2 = parseInt(window.prompt("Enter another number: ")); 
+
+    var answer = addition1 + addition2; 
+
+    alert(answer); 
+}
